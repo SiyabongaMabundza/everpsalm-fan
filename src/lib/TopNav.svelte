8c178ec947b0c0a3d2d58d3d4d95f4a8166ecf24
Siya <slipjar1@gmail.com>
2022-08-13 12:55:51 +0200

    setup Svelte SPA router

---
+++ src/lib/TopNav.svelte
@@ -0,0 +1,29 @@
+<script>
+
+    const navLinks = [
+        {
+            displayName: "Home",
+            href: "#/"
+        },
+        {
+            displayName: "Explore",
+            href: "#/"
+        },
+        {
+            displayName: "More",
+            href: "#/"
+        }
+    ]
+</script>
+<nav class="flex justify-between mx-8 my-4">
+    <div>
+        <a href={navLinks[0].href}>Brand</a>
+    </div>
+    <div>
+        <ul>
+            {#each navLinks as link }
+                <li><a href={link.href}>{link.displayName}</a></li>
+            {/each}
+        </ul>
+    </div>
+</nav>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,7 @@
+<script>
+    import svelteLogo from '../assets/svelte.svg';
+    import Counter from '../lib/Counter.svelte';
+    import TopNav from '../lib/TopNav.svelte';
+</script>
+
+<TopNav />
